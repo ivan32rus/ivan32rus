e216c8e295d868715cb73ce947f66cad6bbd734a
podstolniy_ivan <podstolniyivan@gmail.com>
2022-05-05 07:22:59 +0000

    add node group k8s/terraform

--- k8s/terraform/main.tf
+++ k8s/terraform/main.tf
@@ -43,6 +43,7 @@ resource "yandex_vpc_subnet" "k8s-network" {
 
 resource "yandex_kubernetes_cluster" "k8s-test" {
   network_id = yandex_vpc_network.default1.id
+  version = "1.19"
   master {
     zonal {
       subnet_id = yandex_vpc_subnet.k8s-network.id
@@ -57,4 +58,32 @@ resource "yandex_kubernetes_cluster" "k8s-test" {
       yandex_resourcemanager_folder_iam_binding.editor,
       yandex_resourcemanager_folder_iam_binding.images-puller
      ]
+}
+#--NOW not image packer
+resource "yandex_kubernetes_node_group" "node_groups" {
+  cluster_id  = yandex_kubernetes_cluster.k8s-test.id
+  name        = "k8s-test-group"
+  version     = "1.19"
+
+  instance_template {
+    platform_id = "standard-v2"
+    nat         = true
+    metadata = {
+      ssh-keys = "ubuntu:${var.public_key}"
+    }
+
+    resources {
+      cores  = 4
+      memory = 8
+    }
+    boot_disk {
+      size = 64
+    }
+
+  }
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
+  }
 }
--- k8s/terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "external_ip_address" {
-  value = module.k8s.external_ip_address
-}
